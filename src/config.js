1079708863e9c31c8ec64141c51054f03676b7aa
Rohan Shukla <rohan.shukla-at-252540970514@raweng.com>
2020-06-04 01:04:55 +0530

    Initial Commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteTitle: 'Rohan Shukla | Software Engineer',
+  siteDescription:
+    'Rohan Shukla is a Software Engineer based in Mumbai, India who is specialized in building websites, applications, and everything in between.',
+  siteKeywords:
+    'Rohan Shukla, Rohan, Shukla, rohanshukla, shuklarohan, software engineer, application engineer, full-stack engineer, mern stack, web developer, javascript, mumbai, india',
+  siteUrl: 'https://github.com/shuklarohan',
+  siteLanguage: 'en_US',
+  name: 'Rohan Shukla',
+  location: 'Mumbai, India',
+  email: 'errohanshukla@gmail.com',
+  github: 'https://github.com/shuklarohan'
+};
