5a5c82e62bfbc0966cae2cad47e85dff82cbead7
Rohan Shukla <rohan.shukla-at-252540970514@raweng.com>
2020-06-14 02:50:32 +0530

    Image Setup from Markdown done

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,8 @@ export const pageQuery = graphql`
             skills
             avatar {
               childImageSharp {
-                fluid(maxWidth: 700, quality: 100, traceSVG: { color: "#64ffda" }) {
+                fluid(maxWidth: 700, quality: 100, traceSVG: { color: "rgb(56, 47, 92)", threshold: 75 }
+                      toFormat: JPG) {
                   ...GatsbyImageSharpFluid_withWebp_tracedSVG
                 }
               }
