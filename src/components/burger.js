a69fd7e42ac4d63f25fbc800a6e25160facfc89b
Rohan Shukla <rohan.shukla-at-252540970514@raweng.com>
2020-06-29 01:05:54 +0530

    Burger Width Animation Done

--- src/components/burger.js
+++ src/components/burger.js
@@ -22,15 +22,18 @@ const Container = styled.div`
         border-radius: 10px;
         transform-origin: 1px;
         transition: all 0.3s linear;
+        align-self: flex-end;
 
         &:nth-child(1) {
             transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
         }
         &:nth-child(2) {
+            width: ${({ open }) => open ? '2rem' : '1.6rem'};
             transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
             opacity: ${({ open }) => open ? 0 : 1};
         }
         &:nth-child(3) {
+            width: ${({ open }) => open ? '2rem' : '1.2rem'};
             transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
         }
     }
